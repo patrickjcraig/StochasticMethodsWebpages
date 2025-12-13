34b508bd78deb13cd10db737fefc63b2cc5077a9
patrickjcraig <96743609+patrickjcraig@users.noreply.github.com>
2025-12-13 13:46:58 -0500

    update:

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: "/patrickjcraig.github.io",
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
