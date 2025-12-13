2edbeb30b90ff83de0a379d36dcb428f07e9a8f5
patrickjcraig <96743609+patrickjcraig@users.noreply.github.com>
2025-12-13 14:10:15 -0500

    s

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/patrickjcraig.github.io/",
+  base: "/StochasticMethodsWebpages/",
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
